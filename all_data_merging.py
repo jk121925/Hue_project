4e205c631e897b29b20709b387bccafda91c8cfa
jk121925 <bell1902@naver.com>
2020-07-18 16:11:22 +0900

    rapup

--- all_data_merging.py
+++ all_data_merging.py
@@ -4,12 +4,15 @@ import pandas as pd
 df_standard = pd.read_csv ('fd_origin.csv', encoding = 'cp949')
 df_to_add = pd.read_csv ('fd_2018_5.csv', encoding = 'cp949')
 
+#df_samsung_0 = df_standard[df_standard['code']==5930]
+#df_samsung_1 = df_to_add[df_to_add['code']==5930]
+
 #Extract key value from df_to_add
-df_to_add_key = df_2018[['y2017','y2018']]
+df_to_add_key = df_to_add[['y2017','y2018']]
 
 #df_to_add_listing
 df_to_add_list =[]
-for i in range(len(df_2018_0)):
+for i in range(len(df_to_add)):
     data_2017 = df_to_add_key.iloc[i,0]
     data_2018 = df_to_add_key.iloc[i,1]
     df_to_add_list.append([data_2017,data_2018])
@@ -30,7 +33,7 @@ for i in range(len(df_standard_list)):
     if type(df_standard_list[i]) != list : df_standard_list[i] = [non_data,0]
 
 # setting with copy warning
-df_dummy = df_standard_list.copy()
+df_dummy = df_standard.copy()
 
 # merge data using df_standard
 for i in range(len(df_standard)):
@@ -42,5 +45,4 @@ for i in range(len(df_standard)):
         if origin_index == match_index and df_standard.iloc[i,4] != 'SCF':  df_dummy.iloc[i,8] = new_index
         elif origin_index == match_index and df_standard.iloc[i,4] == 'SCF':  df_dummy.iloc[i,8] = refer_index
 
-# result key is "df_dummy"
-df_dummy.to_excel("all_data_merge.xlsx")
+df_dummy
--- samsung_data_merge.py
+++ samsung_data_merge.py
@@ -1,23 +1,26 @@
 import pandas as pd
 
 #data open using pandas type Dataframe
-df_standard = pd.read_csv ('fd_origin.csv', encoding = 'cp949')
-df_to_add = pd.read_csv ('fd_2018_5.csv', encoding = 'cp949')
+df_standard = pd.read_csv ('./sample_csv/fd_origin.csv', encoding = 'cp949')
+df_to_add = pd.read_csv ('./sample_csv/fd_2018_5.csv', encoding = 'cp949')
+
+df_samsung_0 = df_standard[df_standard['code']==5930]
+df_samsung_1 = df_to_add[df_to_add['code']==5930]
 
 #Extract key value from df_to_add
-df_to_add_key = df_2018[['y2017','y2018']]
+df_to_add_key = df_samsung_1[['y2017','y2018']]
 
 #df_to_add_listing
 df_to_add_list =[]
-for i in range(len(df_2018_0)):
+for i in range(len(df_samsung_1)):
     data_2017 = df_to_add_key.iloc[i,0]
     data_2018 = df_to_add_key.iloc[i,1]
     df_to_add_list.append([data_2017,data_2018])
 
 #df_standard_listing
 df_standard_list = []
-for i in range(len(df_standard)):
-    index_data_2017 = df_standard.iloc[i,7]
+for i in range(len(df_samsung_0)):
+    index_data_2017 = df_samsung_0.iloc[i,7]
     df_standard_list.append(index_data_2017)
 
 #merge data
@@ -30,17 +33,17 @@ for i in range(len(df_standard_list)):
     if type(df_standard_list[i]) != list : df_standard_list[i] = [non_data,0]
 
 # setting with copy warning
-df_dummy = df_standard_list.copy()
+df_dummy = df_samsung_0.copy()
 
 # merge data using df_standard
-for i in range(len(df_standard)):
-    origin_index = df_standard.iloc[i,7]
-    refer_index = df_standard.iloc[i,8]
+for i in range(len(df_samsung_0)):
+    origin_index = df_samsung_0.iloc[i,7]
+    refer_index = df_samsung_0.iloc[i,8]
     for j in range(len(df_standard_list)):
         match_index = df_standard_list[j][0]
         new_index = df_standard_list[j][1]
-        if origin_index == match_index and df_standard.iloc[i,4] != 'SCF':  df_dummy.iloc[i,8] = new_index
-        elif origin_index == match_index and df_standard.iloc[i,4] == 'SCF':  df_dummy.iloc[i,8] = refer_index
+        if origin_index == match_index and df_samsung_0.iloc[i,4] != 'SCF':  df_dummy.iloc[i,8] = new_index
+        elif origin_index == match_index and df_samsung_0.iloc[i,4] == 'SCF':  df_dummy.iloc[i,8] = refer_index
 
 # result key is "df_dummy"
-df_dummy.to_excel("all_data_merge.xlsx")
+df_dummy.to_excel("./samsung_data_merge.xlsx")
